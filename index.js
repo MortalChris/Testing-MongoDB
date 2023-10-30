164740a720a3879767347f3e9c330d063533a4b5
Christian Valeri <mortlaxris@outlook.com>
2023-10-30 13:06:25 -0500

    It works now(changed post to be "/")

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
-const express = require('express');
-const app = express();
+const express = require("express");
+const path = require("path");
 const port = 3000;
-const path = require('path');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/myProject', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+} ,console.log("connected to database"));
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
+
+// Create a schema and model for your data
+const DataModel = mongoose.model('Data', new mongoose.Schema({
+    // Define your data schema here
+    firstName: String,
+    lastName: String,
+    // Add more fields as needed
+}));
+
+// run();
+// async function run() {
+//     const testData = new DataModel({
+//             firstName: "Man",
+//             lastName: "Test"
+//     })
+//     await testData.save();
+//     console.log(testData);
+// }
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+
+app.post("/", async (req, res, next) => {
+    try {
+        const data = new DataModel({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName
+        });
+        const result = await data.save();
+        res.redirect("newPage.html");
+        console.log("result");
+    } catch (err) {
+        console.log("Error")
+        return next(err);
+        
+        };
+});
+
+    app.listen(port, () => {
+        console.log(`Example app listening on port http://localhost:${port}`)
+    });
